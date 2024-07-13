59c2a34730393163fea6089455d2056319e798f6
tmdgus <8571053@naver.com>
2024-07-14 02:02:07 +0900

    할일 CRUD 기능 구현 (feat. Server Action)

--- app/ui.tsx
+++ app/ui.tsx
@@ -1,20 +1,46 @@
 "use client";
 
 import { Button, Input } from "@material-tailwind/react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { createTodo, getTodos } from "actions/todo-actions";
 import Todo from "components/todo";
+import { useState } from "react";
 
 export default function UI() {
+  const [searchInput, setSearchInput] = useState("");
+
+  const todosQuery = useQuery({
+    queryKey: ["todos"],
+    queryFn: () => getTodos({ searchInput }),
+  });
+
+  const createTodoMutation = useMutation({
+    mutationFn: () =>
+      createTodo({
+        title: "New Todo",
+        completed: false,
+      }),
+
+    onSuccess: () => {
+      todosQuery.refetch();
+    },
+  });
+
   return (
     <div className="w-2/3 mx-auto flex flex-col items-center py-10 gap-2">
       <h1 className="text-xl">TODO LIST</h1>
+
       <Input
-        la
-        bel="Search TODO"
+        label="Search TODO"
         placeholder="Search TODO"
         icon={<i className="fas fa-search" />}
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
       />
-      <Todo id={1} value={"New TODO"} completed={true} />
-      <Button>
+
+      {todosQuery.isPending && <p>Loading...</p>}
+      {todosQuery.data && todosQuery.data.map((todo) => <Todo key={todo.id} todo={todo} />)}
+      <Button onClick={() => createTodoMutation.mutate()} loading={createTodoMutation.isPending}>
         <i className="fas fa-plus mr-2" />
         ADD TODO
       </Button>
