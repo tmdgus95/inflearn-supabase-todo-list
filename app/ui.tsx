180ddcd190ecd4305c60d479271f0d9bb36299a9
tmdgus <8571053@naver.com>
2024-07-14 00:48:05 +0900

    UI구축

---
+++ app/ui.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button, Input } from "@material-tailwind/react";
+import Todo from "components/todo";
+
+export default function UI() {
+  return (
+    <div className="w-2/3 mx-auto flex flex-col items-center py-10 gap-2">
+      <h1 className="text-xl">TODO LIST</h1>
+      <Input
+        la
+        bel="Search TODO"
+        placeholder="Search TODO"
+        icon={<i className="fas fa-search" />}
+      />
+      <Todo id={1} value={"New TODO"} completed={true} />
+      <Button>
+        <i className="fas fa-plus mr-2" />
+        ADD TODO
+      </Button>
+    </div>
+  );
+}
